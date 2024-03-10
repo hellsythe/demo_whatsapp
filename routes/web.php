22c00697097bed64185f39bc758f8c188a4bc74c
Camilo Rodriguez <camilo@sdkconsultoria.com>
2024-03-10 15:43:25 -0600

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/messenger', fn() => view('messenger'));
+Route::get('/templates', fn() => view('template'));
